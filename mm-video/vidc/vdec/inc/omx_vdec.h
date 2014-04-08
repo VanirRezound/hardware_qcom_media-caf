6a6be8f8240b498a34f091eb1acdd2a791e01cfe
varchild <sirstreak@gmail.com>
2014-04-07 20:59:41 -0500

    Revert "Merge remote-tracking branch 'lx/cm-11.0' into HEAD"
    This reverts commit 954ae42e9c65723f60f8a814349ba25145834777, reversing
    changes made to 0950420170efc86efa9be3ebbbda8502cd390b2d.

--- mm-video/vidc/vdec/inc/omx_vdec.h
+++ mm-video/vidc/vdec/inc/omx_vdec.h
@@ -875,9 +875,8 @@ private:
              OMX_BUFFERHEADERTYPE **bufferHdr,OMX_U32 port,OMX_PTR appData,
              OMX_U32 bytes);
         OMX_ERRORTYPE free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr);
-        #define MAX_COUNT 32
-        OMX_BUFFERHEADERTYPE  m_out_mem_ptr_client[MAX_COUNT];
     private:
+        #define MAX_COUNT 32
         omx_vdec *omx;
         bool enabled;
         OMX_COLOR_FORMATTYPE ColorFormat;
@@ -891,6 +890,7 @@ private:
         OMX_QCOM_PLATFORM_PRIVATE_LIST      m_platform_list_client[MAX_COUNT];
         OMX_QCOM_PLATFORM_PRIVATE_ENTRY     m_platform_entry_client[MAX_COUNT];
         OMX_QCOM_PLATFORM_PRIVATE_PMEM_INFO m_pmem_info_client[MAX_COUNT];
+        OMX_BUFFERHEADERTYPE  m_out_mem_ptr_client[MAX_COUNT];
         struct vdec_ion op_buf_ion_info[MAX_COUNT];
         unsigned char *pmem_baseaddress[MAX_COUNT];
         int pmem_fd[MAX_COUNT];
